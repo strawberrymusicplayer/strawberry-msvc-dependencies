e1bd1fd9ee6e762d4976707133b5618b5fd2506d
Jonas Kvinge <jonas@jkvinge.net>
2023-07-21 06:36:43 +0200

    Fix libebur128 update

--- scripts/update.sh
+++ scripts/update.sh
@@ -284,7 +284,7 @@ function update_package() {
       package_version_latest=$(wget -q -O- 'https://github.com/knik0/faad2/tags' | sed -n 's#.*releases/tag/\([^"]*\).*#\1#p' | grep -v '^\*name$' | sed 's/_/\./g' | sort -V | tail -1)
       ;;
     "libebur128")
-      package_version_latest=$(wget -q -O- 'https://github.com/jiixyj/libebur128/tags' | sed -n 's#.*releases/tag/\([^"]*\).*#\1#p' | sed 's/^pkgconf\-//g' | sort -V | tail -1)
+      package_version_latest=$(wget -q -O- 'https://github.com/jiixyj/libebur128/tags' | sed -n 's#.*releases/tag/\([^"]*\).*#\1#p' | sed 's/^v//g' | sort -V | tail -1)
       ;;
     *)
       package_version_latest=
