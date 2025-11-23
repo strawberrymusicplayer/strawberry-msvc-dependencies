cb8c889b0dda61b77ae35f59686ad2f97c11d948
Jonas Kvinge <jonas@jkvinge.net>
2025-11-23 18:52:25 +0100

    update: Add update for gmp and nettle

--- scripts/update.sh
+++ scripts/update.sh
@@ -26,7 +26,7 @@ function error() { echo "[$(timestamp)] ERROR: $*" >&2; }
 
 function latest_github_release() {
 
-  curl ${curl_options} -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer ${gh_token}" "https://api.github.com/repos/$1/$2/releases/latest" | jq -r '.tag_name' | sed 's/^v//g' | sed 's/^sparsehash-//g' | sed 's/^faac-//g' | sort -V | head -1
+  curl ${curl_options} -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer ${gh_token}" "https://api.github.com/repos/$1/$2/releases/latest" | jq -r '.tag_name' | sed 's/^v//g' | sed 's/^sparsehash-//g' | sed 's/^faac-//g' | sed 's/^nettle_//g' | sort -V | head -1
 
 }
 
@@ -153,6 +153,12 @@ function update_package() {
       ;;
     "openssl")
       package_version_latest=$(curl ${curl_options} 'https://www.openssl.org/source/' | sed -n 's,.*openssl-\([0-9][0-9a-z.]*\)\.tar.*,\1,p' | sort -V | tail -1)
+      ;;
+    "gmp")
+      package_version_latest=$(latest_github_release "ShiftMediaProject" "gmp")
+      ;;
+    "nettle")
+      package_version_latest=$(latest_github_release "ShiftMediaProject" "nettle")
       ;;
     "gnutls")
       package_version_latest=$(latest_github_release "ShiftMediaProject" "gnutls")
